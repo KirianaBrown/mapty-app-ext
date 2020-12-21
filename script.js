efe1bfafe22c99ba1542495e9d5bf6cc83da676d
KirianaBrown <kjb0369@arastudent.ac.nz>
2020-12-21 16:33:39 +1300

    project set up

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('connected');
+})
